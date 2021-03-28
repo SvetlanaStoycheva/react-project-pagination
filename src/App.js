71b3535c117c96240fbff7d030dde4b1b3ff5159
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-03-28 14:46:29 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import Follower from './Follower'
+function App() {
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    if (loading) {
+      return
+    }
+    setFollowers(data[page])
+  }, [loading, page])
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
+  const handlePrevBtn = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+  const handleNextBtn = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+        <section className='followers'>
+          <div className='container'>
+            {followers.map((follower) => {
+              return <Follower key={follower.id} {...follower} />
+            })}
+          </div>
+          {!loading && (
+            <div className='btn-container'>
+              <button className='prev-btn' onClick={handlePrevBtn}>
+                prev
+              </button>
+              {data.map((item, index) => {
+                return (
+                  <button
+                    className={`${
+                      index === page ? 'active-btn page-btn' : 'page-btn'
+                    }`}
+                    key={index}
+                    onClick={() => {
+                      handlePage(index)
+                    }}
+                  >
+                    {index + 1}
+                  </button>
+                )
+              })}
+              <button className='next-btn' onClick={handleNextBtn}>
+                next
+              </button>
+            </div>
+          )}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
